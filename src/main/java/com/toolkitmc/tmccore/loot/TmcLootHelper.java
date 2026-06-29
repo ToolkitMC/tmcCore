0395b726a578efde2087af35d5e322a9b8badf43
Legends11 <235496468+tickwarden@users.noreply.github.com>
2026-06-29 19:40:48 +0000

    .

--- src/main/java/com/toolkitmc/tmccore/loot/TmcLootHelper.java
+++ src/main/java/com/toolkitmc/tmccore/loot/TmcLootHelper.java
@@ -1,7 +1,6 @@
 package com.toolkitmc.tmccore.loot;
 
 import net.minecraft.loot.LootTable;
-import net.minecraft.loot.context.LootContextParameterSet;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.world.ServerWorld;
@@ -9,7 +8,7 @@ import net.minecraft.util.Identifier;
 
 public class TmcLootHelper {
 
-    public static void dropLoot(ServerWorld world, Identifier lootTableId, LootContextParameterSet params) {
+    public static void dropLoot(ServerWorld world, Identifier lootTableId, LootWorldContext params) {
         RegistryKey<LootTable> key = RegistryKey.of(RegistryKeys.LOOT_TABLE, lootTableId);
         LootTable lootTable = world.getServer().getReloadableRegistries().getLootTable(key);
         lootTable.generateLoot(params, stack -> {
